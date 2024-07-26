9fb341cfdd0f15462031fd9e6516bac5814c1b2e
Marcin Hersztowski <mhersztowski@gmail.com>
2024-07-26 10:33:17 +0200

    add Arduboy2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduboy2.h>
 
+// make an instance of arduboy used for many functions
+Arduboy2 arduboy;
+
+
+// This function runs once in your game.
+// use it for anything that needs to be set only once in your game.
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // initiate arduboy instance
+  arduboy.begin();
+
+  // here we set the framerate to 15, we do not need to run at
+  // default 60 and it saves us battery life
+  arduboy.setFrameRate(15);
 }
 
+
+// our main game loop, this runs once every cycle/frame.
+// this is where our game logic goes.
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // pause render until it's time for the next frame
+  if (!(arduboy.nextFrame()))
+    return;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // first we clear our screen to black
+  arduboy.clear();
+
+  // we set our cursor 5 pixels to the right and 10 down from the top
+  // (positions start at 0, 0)
+  arduboy.setCursor(4, 9);
+
+  // then we print to screen what is in the Quotation marks ""
+  arduboy.print(F("Hello, world!"));
+
+  // then we finaly we tell the arduboy to display what we just wrote to the display
+  arduboy.display();
 }
